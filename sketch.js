0377cf61bf5ae7373e49e6713e23ed47f3cc6715
komal0312 <144373058+komal0312@users.noreply.github.com>
2023-09-28 20:31:11 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -57,11 +57,7 @@ function draw() {
 
 
 function keyPressed() {
-	if (keyCode === UP_ARROW) {	
-		bob1.x = bob1.x+100
-    
-
-  	}
+	if (keyCode === UP_ARROW) { Matter.Body.applyForce(bob1.body,bob1.body.position,{x:-50,y:-45}); }
 }
 
 
